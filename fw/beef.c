d41d40d49df0b2554c07b50959e04bfddcf3f428
Isaac Kim <49662147+supervaka@users.noreply.github.com>
2023-05-22 19:58:31 -0400

    Refactor for consistent style
    * refactor input and led of buttons
    * fix bug in process_button(), refactor Lights_SetState()
    * refactor turntable logic, style changes
    * rename most names to snake_case, remove OUT report struct and just use 16 bitfield for HID led_state
    * convert all indentation to spaces, further refactor turntable logic,

--- fw/beef.c
+++ fw/beef.c
@@ -3,6 +3,28 @@
 
 #include "beef.h"
 
+button_pins buttons[] = {
+  // PIN : PORT : [button no] : [input pin] : [LED pin]  
+  { &PINB, &PORTB, 0, 0, 1 },  // BUTTON 1 : B0 : B1  
+  { &PINB, &PORTB, 1, 2, 3 },  // BUTTON 2 : B2 : B3  
+  { &PINB, &PORTB, 2, 4, 5 },  // BUTTON 3 : B4 : B5  
+  { &PINB, &PORTB, 3, 6, 7 },  // BUTTON 4 : B6 : B7  
+  { &PIND, &PORTD, 4, 0, 1 },  // BUTTON 5 : D0 : D1  
+  { &PIND, &PORTD, 5, 2, 3 },  // BUTTON 6 : D2 : D3  
+  { &PIND, &PORTD, 6, 4, 5 },  // BUTTON 7 : D4 : D5  
+  { &PIND, &PORTD, 7, 6, 7 },  // BUTTON 8 : D6 : D7  
+  { &PINC, &PORTC, 8, 0, 1 },  // BUTTON 9 : C0 : C1  
+  { &PINC, &PORTC, 9, 2, 3 },  // BUTTON 10: C2 : C3
+  { &PINC, &PORTC, 10, 4, 5 }, // BUTTON 11: C4 : C5 
+};
+
+// this assumes that the pins to the 2 DATA lines are on the same port
+// will need to refactor if this is not the case
+// e.g. if a_pin is on F0 and b_pin is on D0
+// PIN : [a_pin] : [b_pin] : [prev] : [tt_position] 
+tt_pins tt_x = { &PINF, 0, 1, -1, 0 };
+// tt_pins tt_y = { &PIN?, ?, ?, -1, 0 };
+
 // buffer to hold the previously generated HID report, for comparison purposes inside the HID class driver.
 static uint8_t PrevJoystickHIDReportBuffer[sizeof(USB_JoystickReport_Data_t)];
 
@@ -23,176 +45,65 @@ USB_ClassInfo_HID_Device_t Joystick_HID_Interface = {
   },
 };
 
-int8_t turntablePosition = 0;
 // bit-field storing button state. bits 0-10 map to buttons 1-11
-uint16_t button = 0;
+uint16_t button_state = 0;
 // flag to represent whether the LEDs are controlled by host or not
 // when not controlled by host, LEDs light up while the corresponding
 // button is held
-bool reactiveLightingMode = true;
+bool reactive_led = true;
 
 int main(void) {
-	SetupHardware();
-	USB_Init();
+  SetupHardware();
+  USB_Init();
 
-	// TT wired to F0/F1
-	DDRF  &= 0b11111100;
-	PORTF |= 0b00000011;
+  // tt_x DATA lines wired to F0/F1
+  DDRF  &= 0b11111100;
+  PORTF |= 0b00000011;
 
-	// buttons mapping:
-	// <name>   : [input pin] : [LED pin]
-	// BUTTON 1 : B0 : B1
-	// BUTTON 2 : B2 : B3
-	// BUTTON 3 : B4 : B5
-	// BUTTON 4 : B6 : B7
-	// BUTTON 5 : D0 : D1
-	// BUTTON 6 : D2 : D3
-	// BUTTON 7 : D4 : D5
-	// START    : D6 : D7
-	// VEFX     : C0 : C1
-	// EFFECT   : C2 : C3
-	// AUX      : C4 : C5
-	DDRB  = 0b10101010;
-	DDRD  = 0b10101010;
-	DDRC &= 0b11101010;
-	DDRC |= 0b00101010;
+  // buttons mapping:
+  // <name>   : [input pin] : [LED pin]
+  // BUTTON 1 : B0 : B1
+  // BUTTON 2 : B2 : B3
+  // BUTTON 3 : B4 : B5
+  // BUTTON 4 : B6 : B7
+  // BUTTON 5 : D0 : D1
+  // BUTTON 6 : D2 : D3
+  // BUTTON 7 : D4 : D5
+  // BUTTON 8 : D6 : D7
+  // BUTTON 9 : C0 : C1
+  // BUTTON 10: C2 : C3
+  // BUTTON 11: C4 : C5
 
-	PORTB  = 0b01010101;
-	PORTD  = 0b01010101;
-	PORTC |= 0b00010101;
-	PORTC &= 0b11010101;
+  DDRB  = 0b10101010;
+  DDRD  = 0b10101010;
+  DDRC &= 0b11101010;
+  DDRC |= 0b00101010;
 
-	int8_t prev = -1;
-	int8_t curr = -1;
+  PORTB  = 0b01010101;
+  PORTD  = 0b01010101;
+  PORTC |= 0b00010101;
+  PORTC &= 0b11010101;
 
-	GlobalInterruptEnable();
+  GlobalInterruptEnable();
 
-	while (1) {
-	  HID_Device_USBTask(&Joystick_HID_Interface);
-	  HID_Task();
-	  USB_USBTask();
+  while (1) {
+    HID_Device_USBTask(&Joystick_HID_Interface);
+    HID_Task();
+    USB_USBTask();
 
-	  // TT logic
-	  // curr is binary number ab
-	  // where a is the signal of F0
-	  // and b is the signal of F1
-	  // e.g. when F0 == 1 and F1 == 0, then curr == 0b10
-	  int8_t a = PINF & (1 << 0) ? 1 : 0;
-	  int8_t b = PINF & (1 << 1) ? 1 : 0;
-	  curr = (a << 1) | b;
+    
+    process_tt(tt_x.PIN, tt_x.a_pin, tt_x.b_pin, &tt_x.prev, &tt_x.tt_position);
+    // process_tt(tt_y.PIN, tt_y.a_pin, tt_y.b_pin, &tt_y.prev, &tt_y.tt_position);
 
-	  if (prev == 3 && curr == 1 ||
-	      prev == 1 && curr == 0 ||
-	      prev == 0 && curr == 2 ||
-	      prev == 2 && curr == 3) {
-	    turntablePosition++;
-	  } else if (prev == 1 && curr == 3 ||
-		     prev == 0 && curr == 1 ||
-		     prev == 2 && curr == 0 ||
-		     prev == 3 && curr == 2) {
-	    turntablePosition--;
-	  }
-	  prev = curr;
-
-	  // BUTTON 1 : B0 : B1
-	  if (~PINB & (1 << 0)) {
-	    button |= (1 << 0);
-	    if (reactiveLightingMode) PORTB |= (1 << 1);
-	  } else {
-	    button &= ~(1 << 0);
-	    if (reactiveLightingMode) PORTB &= ~(1 << 1);
-	  }
-
-	  // BUTTON 2 : B2 : B3
-	  if (~PINB & (1 << 2)) {
-	    button |= (1 << 1);
-	    if (reactiveLightingMode) PORTB |= (1 << 3);
-	  } else {
-	    button &= ~(1 << 1);
-	    if (reactiveLightingMode) PORTB &= ~(1 << 3);
-	  }
-
-	  // BUTTON 3 : B4 : B5
-	  if (~PINB & (1 << 4)) {
-	    button |= (1 << 2);
-	    if (reactiveLightingMode) PORTB |= (1 << 5);
-	  } else {
-	    button &= ~(1 << 2);
-	    if (reactiveLightingMode) PORTB &= ~(1 << 5);
-	  }
-
-	  // BUTTON 4 : B6 : B7
-	  if (~PINB & (1 << 6)) {
-	    button |= (1 << 3);
-	    if (reactiveLightingMode) PORTB |= (1 << 7);
-	  } else {
-	    button &= ~(1 << 3);
-	    if (reactiveLightingMode) PORTB &= ~(1 << 7);
-	  }
-
-	  // BUTTON 5 : D0 : D1
-	  if (~PIND & (1 << 0)) {
-	    button |= (1 << 4);
-	    if (reactiveLightingMode) PORTD |= (1 << 1);
-	  } else {
-	    button &= ~(1 << 4);
-	    if (reactiveLightingMode) PORTD &= ~(1 << 1);
-	  }
-
-	  // BUTTON 6 : D2 : D3
-	  if (~PIND & (1 << 2)) {
-	    button |= (1 << 5);
-	    if (reactiveLightingMode) PORTD |= (1 << 3);
-	  } else {
-	    button &= ~(1 << 5);
-	    if (reactiveLightingMode) PORTD &= ~(1 << 3);
-	  }
-
-	  // BUTTON 7 : D4 : D5
-	  if (~PIND & (1 << 4)) {
-	    button |= (1 << 6);
-	    if (reactiveLightingMode) PORTD |= (1 << 5);
-	  } else {
-	    button &= ~(1 << 6);
-	    if (reactiveLightingMode) PORTD &= ~(1 << 5);
-	  }
-
-	  // START : D6 : D7
-	  if (~PIND & (1 << 6)) {
-	    button |= (1 << 7);
-	    if (reactiveLightingMode) PORTD |= (1 << 7);
-	  } else {
-	    button &= ~(1 << 7);
-	    if (reactiveLightingMode) PORTD &= ~(1 << 7);
-	  }
-
-	  // VEFX : C0 : C1
-	  if (~PINC & (1 << 0)) {
-	    button |= (1 << 8);
-	    if (reactiveLightingMode) PORTC |= (1 << 1);
-	  } else {
-	    button &= ~(1 << 8);
-	    if (reactiveLightingMode) PORTC &= ~(1 << 1);
-	  }
-
-	  // EFFECT : C2 : C3
-	  if (~PINC & (1 << 2)) {
-	    button |= (1 << 9);
-	    if (reactiveLightingMode) PORTC |= (1 << 3);
-	  } else {
-	    button &= ~(1 << 9);
-	    if (reactiveLightingMode) PORTC &= ~(1 << 3);
-	  }
-
-	  // AUX : C4 : C5
-	  if (~PINC & (1 << 4)) {
-	    button |= (1 << 10);
-	    if (reactiveLightingMode) PORTC |= (1 << 5);
-	  } else {
-	    button &= ~(1 << 10);
-	    if (reactiveLightingMode) PORTC &= ~(1 << 5);
-	  }
-	}
+    
+    for (int i = 0; i < 11; ++i) {
+      process_button(buttons[i].PIN, 
+                     buttons[i].PORT,
+                     buttons[i].button_number, 
+                     buttons[i].input_pin, 
+                     buttons[i].led_pin); 
+    }
+  }
 }
 
 // configure board hardware and chip peripherals
@@ -206,7 +117,7 @@ void SetupHardware(void) {
 
   // hardware init
   USB_Init();
-  reactiveLightingMode = true;
+  reactive_led = true;
 }
 
 // event handler for USB connection event
@@ -229,27 +140,26 @@ void EVENT_USB_Device_ControlRequest(void) {
   switch (USB_ControlRequest.bRequest) {
     case HID_REQ_SetReport:
       if (USB_ControlRequest.bmRequestType == (REQDIR_HOSTTODEVICE | REQTYPE_CLASS | REQREC_INTERFACE)) {
-	  Output_t LightsData;
+        uint16_t led_state;
 
-	  Endpoint_ClearSETUP();
+        Endpoint_ClearSETUP();
 
-	  // read report data from the control endpoint
-	  Endpoint_Read_Control_Stream_LE(&LightsData, sizeof(LightsData));
-	  Endpoint_ClearIN();
+        // read report data from the control endpoint
+        Endpoint_Read_Control_Stream_LE(&led_state, sizeof(led_state));
+        Endpoint_ClearIN();
 
-	  ProcessGenericHIDReport(&LightsData);
-	}
-
-      break;
-    }
+        ProcessGenericHIDReport(led_state);
+      }
+    break;
+  }
 }
 
 // process last received report from the host.
-void ProcessGenericHIDReport(Output_t* ReportData) {
-  reactiveLightingMode = false;
+void ProcessGenericHIDReport(uint16_t led_state) {
+  reactive_led = false;
 
   //forward the lighting data
-  Lights_SetState(ReportData->Lights);
+  update_lighting(led_state);
 }
 
 void HID_Task(void) {
@@ -260,13 +170,13 @@ void HID_Task(void) {
     // check if packet contains data
     if (Endpoint_IsReadWriteAllowed()) {
       //temp buffer to hold the read in report from the host
-      Output_t LightsData;
+      uint16_t led_state;
 
       // read generic report data
-      Endpoint_Read_Stream_LE(&LightsData, sizeof(LightsData), NULL);
+      Endpoint_Read_Stream_LE(&led_state, sizeof(led_state), NULL);
 
       // process generic report data
-      ProcessGenericHIDReport(&LightsData);
+      ProcessGenericHIDReport(led_state);
     }
 
     // finalize the stream transfer to send the last packet
@@ -282,8 +192,9 @@ bool CALLBACK_HID_Device_CreateHIDReport(USB_ClassInfo_HID_Device_t* const HIDIn
                                          uint16_t* const ReportSize) {
   USB_JoystickReport_Data_t* JoystickReport = (USB_JoystickReport_Data_t*)ReportData;
 
-  JoystickReport->X = turntablePosition;
-  JoystickReport->Button = button;
+  JoystickReport->X = tt_x.tt_position;
+  // JoystickReport->Y = tt_y.tt_position;
+  JoystickReport->Button = button_state; 
 
   *ReportSize = sizeof(USB_JoystickReport_Data_t);
   return false;
@@ -293,78 +204,69 @@ void CALLBACK_HID_Device_ProcessHIDReport(USB_ClassInfo_HID_Device_t* const HIDI
                                           const uint8_t ReportID,
                                           const uint8_t ReportType,
                                           const void* ReportData,
-                                          const uint16_t ReportSize)
-{
+                                          const uint16_t ReportSize) {
   // Unused (but mandatory for the HID class driver) in this demo, since there are no Host->Device reports
 }
 
-void Lights_SetState(uint16_t OutputData) {
-  // buttons to pins:
-  // <name>   : <input pin> : <LED pin>
-  // BUTTON 1 : B0 : B1
-  if (OutputData & (1 << 0)) {
-    PORTB |= (1 << 1);
+void set_led(volatile uint8_t* PORT, 
+             uint8_t button_number, 
+             uint8_t led_pin, 
+             uint16_t led_state) {
+  if (led_state & (1 << button_number)) {
+    *PORT |= (1 << led_pin);
   } else {
-    PORTB &= ~(1 << 1);
+    *PORT &= ~(1 << led_pin);
   }
-  // BUTTON 2 : B2 : B3
-  if (OutputData & (1 << 1)) {
-    PORTB |= (1 << 3);
+}
+
+void process_button(volatile uint8_t* PIN, 
+                    volatile uint8_t* PORT, 
+                    uint8_t button_number, 
+                    uint8_t input_pin, 
+                    uint8_t led_pin) {
+  if (~*PIN & (1 << input_pin)) {
+    button_state |= (1 << button_number);
+    if (reactive_led) *PORT |= (1 << led_pin);
   } else {
-    PORTB &= ~(1 << 3);
+    button_state &= ~(1 << button_number);
+    if (reactive_led) *PORT &= ~(1 << led_pin);
   }
-  // BUTTON 3 : B4 : B5
-  if (OutputData & (1 << 2)) {
-    PORTB |= (1 << 5);
-  } else {
-    PORTB &= ~(1 << 5);
+}
+
+void process_tt(volatile uint8_t* PIN, 
+                uint8_t a_pin, 
+                uint8_t b_pin,
+                int8_t* prev, 
+                int8_t* tt_position) {
+  // tt logic
+  // example where tt_x wired to F0/F1:
+  // curr is binary number ab
+  // where a is the signal of F0
+  // and b is the signal of F1
+  // therefore when F0 == 1 and F1 == 0, then curr == 0b10
+  int8_t a = *PIN & (1 << a_pin) ? 1 : 0;
+  int8_t b = *PIN & (1 << b_pin) ? 1 : 0;
+  int8_t curr = (a << 1) | b;
+
+  if (*prev == 3 && curr == 1 ||
+      *prev == 1 && curr == 0 ||
+      *prev == 0 && curr == 2 ||
+      *prev == 2 && curr == 3) {
+    (*tt_position)++;
+  } else if (*prev == 1 && curr == 3 ||
+             *prev == 0 && curr == 1 ||
+             *prev == 2 && curr == 0 ||
+             *prev == 3 && curr == 2) {
+    (*tt_position)--;
   }
-  // BUTTON 4 : B6 : B7
-  if (OutputData & (1 << 3)) {
-    PORTB |= (1 << 7);
-  } else {
-    PORTB &= ~(1 << 7);
-  }
-  // BUTTON 5 : D0 : D1
-  if (OutputData & (1 << 4)) {
-    PORTD |= (1 << 1);
-  } else {
-    PORTD &= ~(1 << 1);
-  }
-  // BUTTON 6 : D2 : D3
-  if (OutputData & (1 << 5)) {
-    PORTD |= (1 << 3);
-  } else {
-    PORTD &= ~(1 << 3);
-  }
-  // BUTTON 7 : D4 : D5
-  if (OutputData & (1 << 6)) {
-    PORTD |= (1 << 5);
-  } else {
-    PORTD &= ~(1 << 5);
-  }
-  // START    : D6 : D7
-  if (OutputData & (1 << 7)) {
-    PORTD |= (1 << 7);
-  } else {
-    PORTD &= ~(1 << 7);
-  }
-  // VEFX     : C0 : C1
-  if (OutputData & (1 << 8)) {
-    PORTC |= (1 << 1);
-  } else {
-    PORTC &= ~(1 << 1);
-  }
-  // EFFECT   : C2 : C3
-  if (OutputData & (1 << 9)) {
-    PORTC |= (1 << 3);
-  } else {
-    PORTC &= ~(1 << 3);
-  }
-  // AUX      : C4 : C5
-  if (OutputData & (1 << 10)) {
-    PORTC |= (1 << 5);
-  } else {
-    PORTC &= ~(1 << 5);
+  *prev = curr;
+}
+
+void update_lighting(uint16_t led_state) {
+  for (int i = 0; i < 11; ++i) {
+    set_led(buttons[i].PORT, 
+            buttons[i].button_number, 
+            buttons[i].led_pin, 
+            led_state);  
   }
 }
--- fw/beef.h
+++ fw/beef.h
@@ -24,12 +24,22 @@ typedef struct {
   uint16_t Button; // bit-field representing which buttons have been pressed
 } USB_JoystickReport_Data_t;
 
-// The OUT reprot structure
+// To bundle each button to its respective pins
 typedef struct {
-  uint16_t Lights; // 16 bits for 16 lights
-  uint8_t  Command;
-  uint8_t  Data;
-} Output_t;
+  volatile uint8_t* PIN;
+  volatile uint8_t* PORT;
+  uint8_t button_number;
+  uint8_t input_pin;
+  uint8_t led_pin;
+} button_pins; 
+
+typedef struct {
+  volatile uint8_t* PIN;
+  uint8_t a_pin;
+  uint8_t b_pin;
+  int8_t prev;
+  int8_t tt_position;
+} tt_pins; 
 
 void SetupHardware(void);
 void HID_Task(void);
@@ -38,15 +48,29 @@ void EVENT_USB_Device_Disconnect(void);
 void EVENT_USB_Device_ConfigurationChanged(void);
 void EVENT_USB_Device_ControlRequest(void);
 void EVENT_USB_Device_StartOfFrame(void);
-void ProcessGenericHIDReport(Output_t* ReportData);
-void Lights_SetState(uint16_t OutputData);
+void ProcessGenericHIDReport(uint16_t led_state);
+void set_led(volatile uint8_t* PORT, 
+             uint8_t button_number, 
+             uint8_t led_pin, 
+             uint16_t OutputData);
+void process_button(volatile uint8_t* PIN, 
+                    volatile uint8_t* PORT, 
+                    uint8_t button_number, 
+                    uint8_t input_pin, 
+                    uint8_t led_pin);
+void process_tt(volatile uint8_t* PIN, 
+                uint8_t a_pin, 
+                uint8_t b_pin,
+                int8_t* prev, 
+                int8_t* tt_position);
+void update_lighting(uint16_t led_data);
 bool CALLBACK_HID_Device_CreateHIDReport(USB_ClassInfo_HID_Device_t* const HIDInterfaceInfo,
-					 uint8_t* const ReportID,
-					 const uint8_t ReportType,
-					 void* ReportData,
-					 uint16_t* const ReportSize);
+                                         uint8_t* const ReportID,
+                                         const uint8_t ReportType,
+                                         void* ReportData,
+                                         uint16_t* const ReportSize);
 void CALLBACK_HID_Device_ProcessHIDReport(USB_ClassInfo_HID_Device_t* const HIDInterfaceInfo,
-					  const uint8_t ReportID,
-					  const uint8_t ReportType,
-					  const void* ReportData,
-					  const uint16_t ReportSize);
+                                          const uint8_t ReportID,
+                                          const uint8_t ReportType,
+                                          const void* ReportData,
+                                          const uint16_t ReportSize); 
